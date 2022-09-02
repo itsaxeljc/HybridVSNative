9072b021cc1822dd060ab29b4e8d8ba7de7e550c
Bradley Addiel González Flores <83793140+bradwick7@users.noreply.github.com>
2022-09-01 19:10:13 -0600

    Inicio (1/4)

--- nativevshybrid/src/components/inicio/inicio.jsx
+++ nativevshybrid/src/components/inicio/inicio.jsx
@@ -14,6 +14,26 @@ export function Inicio(props) {
         <div className={styles.input}>
           <input type="text" placeholder="Ingrese tu nombre de jugador" />
         </div>
+        <div className={styles.button}>Iniciar juego</div>
+        <div className={styles.scoreboard}>
+          <span className={styles.subtitle}>Top 3</span>
+          <div className={styles.players}>
+            <table>
+              <tr>
+                <td className={styles.player}>Pedro Avila</td>
+                <td className={styles.score}>10pts</td>
+              </tr>
+              <tr>
+                <td className={styles.player}>Bradley Flores</td>
+                <td className={styles.score}>9pts</td>
+              </tr>
+              <tr>
+                <td className={styles.player}>Axel Jacobo</td>
+                <td className={styles.score}>9pts</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
