8d2ba0762d8de331efd224fe2cab7289e1541a8a
Bradley Addiel González Flores <83793140+bradwick7@users.noreply.github.com>
2022-09-01 18:17:33 -0600

    update inicio

--- nativevshybrid/src/components/inicio/inicio.jsx
+++ nativevshybrid/src/components/inicio/inicio.jsx
@@ -1,18 +1,20 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 import styles from "./inicio.module.scss";
-import Spline from '@splinetool/react-spline';
-
-
+import Spline from "@splinetool/react-spline";
 
 export function Inicio(props) {
-
-
-    return (
-        <div className="mainContainer">
-            <div>
-            <Spline className={styles.spline} scene="https://prod.spline.design/jyYz8JohihQqSFXV/scene.splinecode" />
-            </div>
+  return (
+    <div className={styles.mainContainer}>
+      <Spline
+        className={styles.spline}
+        scene="https://prod.spline.design/jyYz8JohihQqSFXV/scene.splinecode"
+      />
+      <div className={styles.wrapper}>
+        <span className={styles.title}>Hybrid vs Native</span>
+        <div className={styles.input}>
+          <input type="text" placeholder="Ingrese tu nombre de jugador" />
         </div>
-    );
-
-};
+      </div>
+    </div>
+  );
+}
