ea7028acd19d22e16f22706af90ec0a365e279f6
Bradley Addiel González Flores <83793140+bradwick7@users.noreply.github.com>
2022-09-03 16:58:04 -0600

    Mostrar resumen

--- nativevshybrid/src/components/ending/ending.jsx
+++ nativevshybrid/src/components/ending/ending.jsx
@@ -1,28 +1,111 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 import styles from "./ending.module.scss";
 import baseStyle from "../pages/pages.module.scss";
-import {Pages} from "../pages/pages";
-
+import { Pages } from "../pages/pages";
 
 export function Ending(props) {
-    const points = props.points;
-    const answers = props.answers;
-    const [show, setShow] = useState(true);
-    const [showI, setShowI] = useState(false);
+  const questions = [
+    "Mayor seguridad",
+    "Mayor rapidez",
+    "Mejor integración",
+    "Una sola base de código",
+    "Mejor experiencia de usuario",
+    "Menor costo",
+    "Tiempo de desarrollo más corto",
+    "Alto nivel de personalización",
+    "Mantenimiento complejo",
+    "Escalabilidad",
+  ];
 
-    return (
+  const respuestas = [
+    "Nativa",
+    "Nativa",
+    "Nativa",
+    "Híbrida",
+    "Nativa",
+    "Híbrida",
+    "Híbrida",
+    "Nativa",
+    "Nativa",
+    "Híbrida",
+  ];
+
+  const points = props.points;
+  const answers = props.answers;
+  const [question, setQuestion] = useState(0);
+  const [show, setShow] = useState(true);
+  const [showI, setShowI] = useState(false);
+
+  function changeQuestion(i) {
+    if (i == 9) {
+      setQuestion(0);
+    } else {
+      setQuestion(i + 1);
+    }
+  }
+
+  return (
     <div className={baseStyle.mainContainer}>
       {show ? (
         <div className={baseStyle.wrapper}>
           <span className={baseStyle.title}>
-            ¡Has obtenido<br/>{points} puntos!
+            ¡Has obtenido
+            <br />
+            {points} puntos!
           </span>
-          <span className={styles.ins}>
-            <p>
-              Aquí tienes un resumen de tus respuestas
-              {console.log(answers)}
-            </p>
-          </span>
+          <div className={styles.ins}>
+            {questions.map((item, i) => {
+              return (
+                <div
+                  className={
+                    question === i
+                      ? styles.questionActive
+                      : styles.questionHidden
+                  }
+                  key={i}
+                >
+                  <p>Aquí tienes un resumen de tus respuestas:</p>
+                  <p>
+                    Pregunta {i + 1}
+                    <br />
+                    {item}
+                    <br />
+                    Respondiste: {answers[i]}
+                  </p>
+
+                  <div
+                    className={
+                      answers[i] === respuestas[i]
+                        ? styles.active
+                        : styles.questionHidden
+                    }
+                  >
+                    <span className={styles.prettyFont}>
+                      ¡Tu respuesta es correcta!
+                    </span>
+                  </div>
+
+                  <div
+                    className={
+                      answers[i] !== respuestas[i]
+                        ? styles.active
+                        : styles.questionHidden
+                    }
+                  >
+                    <span className={styles.prettyFont}>
+                      ¡Tu respuesta es incorrecta!
+                    </span>
+                  </div>
+                  <div
+                    onClick={() => changeQuestion(i)}
+                    className={styles.siguiente}
+                  >
+                    Siguiente {">"}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
           <div
             className={baseStyle.button}
             onClick={() => {
@@ -34,8 +117,7 @@ export function Ending(props) {
           </div>
         </div>
       ) : null}
-      {showI ? <Pages/>:null}
-      </div>
-    );
-
-};
+      {showI ? <Pages /> : null}
+    </div>
+  );
+}
--- nativevshybrid/src/components/pregunta/pregunta.jsx
+++ nativevshybrid/src/components/pregunta/pregunta.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import styles from "./pregunta.module.scss";
 import baseStyle from "../pages/pages.module.scss";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import {Ending} from '../ending/ending';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { Ending } from "../ending/ending";
 
 export function Pregunta(props) {
   const preguntas = props.preguntas;
@@ -15,40 +15,39 @@ export function Pregunta(props) {
   const [question, setQuestion] = useState(preguntas[0]);
   const [count, setCount] = useState(1);
   const [showFinal, setShowFinal] = useState(false);
-  const [seconds,setSeconds] = useState(5);
-  const [points,setPoints] = useState(0);
+  const [seconds, setSeconds] = useState(5);
+  const [points, setPoints] = useState(0);
   var timer;
 
-  const [answers,setAnswers] = useState([]);
+  const [answers, setAnswers] = useState([]);
 
   //Timer controls
-  useEffect(()=>{
-    timer = setInterval(()=>{
-      setSeconds(seconds-1);
-      if(seconds===0){
+  useEffect(() => {
+    timer = setInterval(() => {
+      setSeconds(seconds - 1);
+      if (seconds === 0) {
         setCount(count + 1);
         setQuestion(preguntas[count]);
         setSeconds(temps);
       }
-      if(count===preguntas.length+1){
+      if (count === preguntas.length + 1) {
         setShow(!show);
         restart();
         stop();
         setCount(1);
         setShowFinal(!showFinal);
       }
-    },1000)
-      return () => clearInterval(timer);
+    }, 1000);
+    return () => clearInterval(timer);
   });
 
-  const restart=() => {
+  const restart = () => {
     setSeconds(temps);
-  }
+  };
 
-  const stop=() => {
+  const stop = () => {
     clearInterval(timer);
-  }
-
+  };
 
   return (
     <div className={baseStyle.mainContainer}>
@@ -59,59 +58,69 @@ export function Pregunta(props) {
           </div>
           <div className={baseStyle.timer}>
             <div className={styles.timerIcon}>
-              <CircularProgressbar value={seconds} maxValue={temps}
-              styles={{
-                path: { 
-                  stroke: '#358BB1'
-                },
-                text: { 
-                  fill: '#ffffff',
-                  fontSize: '16px'
-                }
-              }}/>
+              <CircularProgressbar
+                value={seconds}
+                maxValue={temps}
+                styles={{
+                  path: {
+                    stroke: "#358BB1",
+                  },
+                  text: {
+                    fill: "#ffffff",
+                    fontSize: "16px",
+                  },
+                }}
+              />
             </div>
           </div>
           <span className={baseStyle.title} style={style}>
             {question}
           </span>
           <div className={styles.options}>
-            <div className={baseStyle.button} onClick={() => {
-              setPoints(points+(seconds*respuestasNativa[count-1]));
-              console.log(answers);
-              setAnswers(answers => answers.concat("Nativa"));
-              restart();
-              setCount(count+1);
-              if(count===preguntas.length){
-                setShow(!show);
+            <div
+              className={baseStyle.button}
+              onClick={() => {
+                setPoints(points + seconds * respuestasNativa[count - 1]);
+                console.log(answers);
+                setAnswers((answers) => answers.concat("Nativa"));
                 restart();
-                stop();
-                setCount(1);
-                setShowFinal(!showFinal);
-              }
-              setQuestion(preguntas[count]);
-            }}>Nativa</div>
-            <div className={baseStyle.button} onClick={() => {
-              setPoints(points+(seconds*respuestasHibrida[count-1]));
-              console.log(answers);
-              setAnswers(answers => answers.concat("Híbrida"));
-              restart();
-              setCount(count+1);
-              if(count===preguntas.length){
-                setShow(!show);
+                setCount(count + 1);
+                if (count === preguntas.length) {
+                  setShow(!show);
+                  restart();
+                  stop();
+                  setCount(1);
+                  setShowFinal(!showFinal);
+                }
+                setQuestion(preguntas[count]);
+              }}
+            >
+              Nativa
+            </div>
+            <div
+              className={baseStyle.button}
+              onClick={() => {
+                setPoints(points + seconds * respuestasHibrida[count - 1]);
+                console.log(answers);
+                setAnswers((answers) => answers.concat("Híbrida"));
                 restart();
-                stop();
-                setCount(1);
-                setShowFinal(!showFinal);
-              }
-              setQuestion(preguntas[count]);
-            }}>Híbrida</div>
-            
+                setCount(count + 1);
+                if (count === preguntas.length) {
+                  setShow(!show);
+                  restart();
+                  stop();
+                  setCount(1);
+                  setShowFinal(!showFinal);
+                }
+                setQuestion(preguntas[count]);
+              }}
+            >
+              Híbrida
+            </div>
           </div>
         </div>
       ) : null}
-      {
-        showFinal ? <Ending points={points} answers={answers}/> : null
-      }
+      {showFinal ? <Ending points={points} answers={answers} /> : null}
     </div>
   );
 }
