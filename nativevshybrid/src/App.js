ca349ca9084ae36476de90ddfcfb664d37fc898c
Pedro <pedrol359.avila.b@gmail.com>
2022-09-02 23:33:58 -0500

    connection with firebase

--- nativevshybrid/src/App.js
+++ nativevshybrid/src/App.js
@@ -1,6 +1,16 @@
 import './App.scss';
 import {Inicio} from './components/inicio/inicio';
+// import { insertNewTop, getUser} from './services/firebaseDB';
 
+
+// const newTop={
+//   nombre:'Pedro',
+//   top:'1',
+//   points:10,
+//   pointsRef:120,
+// };
+// insertNewTop(newTop);
+// getUser();
 function App() {
   return (
     <Inicio></Inicio>
---
+++ nativevshybrid/src/services/firebaseDB.js
@@ -0,0 +1,30 @@
+
+import { db } from "./firebase-config";
+import {collection, doc,setDoc} from 'firebase/firestore';
+
+// export async function getUser(){
+//     const docRef = doc(db,'QuizGamesTop','1');
+//     const res = await getDoc(docRef);
+//     console.log( res );
+// }
+
+export async function getUser(){
+    const citiesRef = collection (db,'QuizGamesTop');
+    const snapshot = await citiesRef.get();
+    snapshot.forEach(data => {
+      console.log(data.id, '=>', data.data());
+    });
+}
+
+
+export async function insertNewTop(newTop){
+    try{
+        const collectionRef = collection (db,'QuizGamesTop');
+        const docRef = doc( collectionRef, newTop.top);
+        const resp = await setDoc (docRef, newTop);
+        console.log(resp);
+    } catch(e){
+        console.log(e); 
+    }
+
+}
